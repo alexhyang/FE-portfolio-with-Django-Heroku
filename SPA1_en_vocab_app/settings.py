878b314485f324f9bb50139a3b13c1e8d8959d13
Alex <subscription.shaman@gmail.com>
2021-02-17 15:35:31 -0800

    updated project settings for deployment

--- SPA1_en_vocab_app/settings.py
+++ SPA1_en_vocab_app/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,7 +26,7 @@ SECRET_KEY = 'nfo6yf_5a+8yn#t85ir*r@n)_k7nn^-q2v2&vdyfcvw-o6!--7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['en-vocabulary-app.herokuapp.com']
 
 
 # Application definition
@@ -121,6 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+## modified ## initially there is only STATIC_URL defined
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
