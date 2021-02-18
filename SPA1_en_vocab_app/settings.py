ce266703fbf5dc760484fd9c3af1b118ce1995ee
Alex <subscription.shaman@gmail.com>
2021-02-18 13:22:30 -0800

    updated settings.py

--- SPA1_en_vocab_app/settings.py
+++ SPA1_en_vocab_app/settings.py
@@ -14,10 +14,10 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'SPA1_en_vocab_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
