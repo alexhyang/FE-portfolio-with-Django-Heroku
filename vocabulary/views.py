a8362ea0571c1674ff4a7a330349ed5ca2aee973
Alex <subscription.shaman@gmail.com>
2021-02-23 18:37:38 -0800

    added user related functionalities

--- vocabulary/views.py
+++ vocabulary/views.py
@@ -1,5 +1,60 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
+from django.db import IntegrityError
 from django.shortcuts import render
+from django.urls import reverse
+
+from .models import User, WordList, Word
 
 # Create your views here.
 def index(request):
     return render(request, "vocabulary/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "vocabulary/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "vocabulary/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "vocabulary/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "vocabulary/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "vocabulary/register.html")
