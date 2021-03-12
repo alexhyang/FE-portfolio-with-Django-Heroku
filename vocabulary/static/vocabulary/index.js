572a55bab69172084f681d8bca2b2ba451b87d60
Alex <subscription.shaman@gmail.com>
2021-03-12 15:36:18 -0800

    cleaned index page html and js code

--- vocabulary/static/vocabulary/index.js
+++ vocabulary/static/vocabulary/index.js
@@ -1,21 +1,14 @@
 $(function () {
-
-	// count result
-	$("#count").on("click", () => {
-		const inputText = $("#textarea").val();
-		if (inputText.length > 0) {
-			$('#result-section').show();
-			updateTotalNumber(inputText, $("#counter"));
-			showTable(inputText, $("#freq-table"));
-		} else {
-			alert("Please input valid text!");
-		}
-		$('#result').val(inputText);
-		return false; //prevent page reload after button click
-	});
-
-	// clear text
-	$("#clear").on("click", () => {
-		$("#textarea").val("");
-	});
+  $("#count").on("click", () => {
+    const inputText = $("#textarea").val();
+    if (inputText.length > 0) {
+      $("#result-section").show();
+      updateTotalNumber(inputText, $("#counter"));
+      showTable(inputText, $("#freq-table"));
+    } else {
+      alert("Please input valid text!");
+    }
+    $("#result").val(inputText);
+    return false; //prevent page reload after button click
+  });
 });
