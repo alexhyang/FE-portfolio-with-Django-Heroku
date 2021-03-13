4a303b1dc2ea514942a4ebc5886d5890c941de88
Alex <subscription.shaman@gmail.com>
2021-03-13 00:42:12 -0800

    added string representation for Settings database

--- vocabulary/models.py
+++ vocabulary/models.py
@@ -5,29 +5,42 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+
 class WordDict(models.Model):
     word = models.CharField(max_length=32)
     meaning = models.TextField(blank=True)
-    
+
     def __str__(self):
         return f"{self.word}"
+
 
 class WordList(models.Model):
     name = models.CharField(max_length=64)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wordlist") #User.wordlist --> many wordlists to one user
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="wordlist"
+    )  # User.wordlist --> many wordlists to one user
     description = models.TextField(blank=True)
-    
+
     def __str__(self):
         return f"{self.name}"
 
+
 class Word(models.Model):
     word = models.CharField(max_length=32)
-    wordlists = models.ManyToManyField(WordList, blank=True, related_name="words") #Wordlist.word --> many words to many wordlists
-    users = models.ManyToManyField(User, related_name="words") #User.word --> many words to many users
-    
+    wordlists = models.ManyToManyField(
+        WordList, blank=True, related_name="words"
+    )  # Wordlist.word --> many words to many wordlists
+    users = models.ManyToManyField(
+        User, related_name="words"
+    )  # User.word --> many words to many users
+
     def __str__(self):
         return f"{self.word}"
-    
+
+
 class Settings(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="settings")
     list_card_uppercase = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username}: settings"
