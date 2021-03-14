cb17aad89deefb8db04fad11d061dc444c07ef72
Alex <subscription.shaman@gmail.com>
2021-03-13 18:25:54 -0800

    improved index page UI

--- vocabulary/static/vocabulary/index.js
+++ vocabulary/static/vocabulary/index.js
@@ -2,13 +2,22 @@ $(function () {
   $("#count").on("click", () => {
     const inputText = $("#textarea").val();
     if (inputText.length > 0) {
-      $("#result-section").show();
-      updateTotalNumber(inputText, $("#counter"));
+      $("#index-result-col").show();
+      updateTotalNumber(inputText, $("#word-counter"));
       showTable(inputText, $("#freq-table"));
     } else {
       alert("Please input valid text!");
     }
     $("#result").val(inputText);
+
+    $("#introduction")
+      .addClass("justify-content-between")
+      .removeClass("justify-content-center");
+
+    $("#freq-counter-app")
+      .addClass("justify-content-between")
+      .removeClass("justify-content-center");
+
     return false; //prevent page reload after button click
   });
 });
