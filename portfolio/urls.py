5eb3db7557aee8e5b5b44b440ca083b6a289f05a
Alex <subscription.shaman@gmail.com>
2021-03-31 16:42:18 -0700

    updated url settings

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('vocabulary.urls')),
+    path('app/', include('vocabulary.urls')),
     path('', include('showcase.urls'))
 ]
--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("en-vocab-app/", views.index, name="index"),
-    path("en-vocab-app/login", views.login_view, name="login"),
-    path("en-vocab-app/logout", views.logout_view, name="logout"),
-    path("en-vocab-app/sign_up", views.register, name="register"),
-    path("en-vocab-app/save", views.save, name="save"),
-    path("en-vocab-app/lists", views.manage_lists, name="manage_lists"),
-    path("en-vocab-app/lists/add_list", views.add_list, name="add_list"),
-    path("en-vocab-app/lists/<str:name>", views.wordlist, name="wordlist"),
-    path("en-vocab-app/lists/<str:name>/remove", views.remove_list, name="remove_list")
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("sign_up", views.register, name="register"),
+    path("save", views.save, name="save"),
+    path("lists", views.manage_lists, name="manage_lists"),
+    path("lists/add_list", views.add_list, name="add_list"),
+    path("lists/<str:name>", views.wordlist, name="wordlist"),
+    path("lists/<str:name>/remove", views.remove_list, name="remove_list")
 ]
