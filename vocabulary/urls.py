8514763724fe562229deed9a410a1389ca17afdd
Alex <subscription.shaman@gmail.com>
2021-03-30 20:25:11 -0700

    updated settings and urls after renaming project

--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("app/", views.index, name="index"),
-    path("app/login", views.login_view, name="login"),
-    path("app/logout", views.logout_view, name="logout"),
-    path("app/sign_up", views.register, name="register"),
-    path("app/save", views.save, name="save"),
-    path("app/lists", views.manage_lists, name="manage_lists"),
-    path("app/lists/add_list", views.add_list, name="add_list"),
-    path("app/lists/<str:name>", views.wordlist, name="wordlist"),
-    path("app/lists/<str:name>/remove", views.remove_list, name="remove_list")
+    path("en-vocab-app/", views.index, name="index"),
+    path("en-vocab-app/login", views.login_view, name="login"),
+    path("en-vocab-app/logout", views.logout_view, name="logout"),
+    path("en-vocab-app/sign_up", views.register, name="register"),
+    path("en-vocab-app/save", views.save, name="save"),
+    path("en-vocab-app/lists", views.manage_lists, name="manage_lists"),
+    path("en-vocab-app/lists/add_list", views.add_list, name="add_list"),
+    path("en-vocab-app/lists/<str:name>", views.wordlist, name="wordlist"),
+    path("en-vocab-app/lists/<str:name>/remove", views.remove_list, name="remove_list")
 ]
