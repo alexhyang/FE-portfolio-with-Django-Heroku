9e92d1986974df31c1ec0cc2238d2d480c89941b
Alex <subscription.shaman@gmail.com>
2021-03-30 20:54:17 -0700

    updated project name

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn en_vocab_app.wsgi
+web: gunicorn portfolio.wsgi
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,4 +1,4 @@
-"""en_vocab_app URL Configuration
+"""portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
