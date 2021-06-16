61e0b16e332cc51720cc4cd39445a76fac0047de
Alex <subscription.shaman@gmail.com>
2021-06-15 21:59:46 -0700

    initialized testing files

---
+++ vocabulary/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.db.models import Max
+
+from vocabulary.models import Oxford, Word, WordList, User
+
+# Create your tests here.
+class WordTestCase(TestCase):
+    def setUp(self):
+        # create user
+        u1 = User.objects.create_user(username="u1", email="u1@test.com", password="u1password")
+        
+        # create word list
+        wl1 = WordList.objects.create(name="test_list", owner=u1, description="First list")
+        
+        # create word
+        word1 = Word.objects.create(word="test", wordlist=wl1, user=u1)
+        
+    def test_wordlist_count(self):
+        u = User.objects.get(username="u1")
+        self.assertEqual(u.wordlist.count(), 1)
