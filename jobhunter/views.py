142eeaaa43dcf2f2d7d152635a1e0122f9aa5095
Alex Yang <alex.hyang.dev@gmail.com>
2021-07-16 21:43:20 -0700

    (vocabulary) scripts minor improvement

--- jobhunter/views.py
+++ jobhunter/views.py
@@ -25,37 +25,16 @@ def add(request):
                 messages.error(request, "This posting already exists!")
                 return render(request, "jobhunter/add.html", {"form": form})
             else:
-                position = form.cleaned_data["position"]
-                level = form.cleaned_data["level"]
-                type = form.cleaned_data["type"]
-                due_date = form.cleaned_data["due_date"]
-                responsibilities = form.cleaned_data["responsibilities"]
-                qualifications = form.cleaned_data["qualifications"]
-                skills = form.cleaned_data["skills"]
-                place = form.cleaned_data["place"]
-                other = form.cleaned_data["other"]
-
-                posting = Posting.objects.create(
-                    position=position,
-                    level=level,
-                    type=type,
-                    url=url,
-                    due_date=due_date,
-                    responsibilities=responsibilities,
-                    qualifications=qualifications,
-                    skills=skills,
-                    company=company,
-                    place=place,
-                    other=other
-                )
+                posting = Posting.objects.create(**form.cleaned_data)
                 posting.save()
                 messages.success(request, "Posting added successfully!")
                 return redirect("jobhunter:index")
-        
+
         else:
             return render(request, "jobhunter/add.html", {"form": form})
     else:
         return render(request, "jobhunter/add.html", {"form": PostingForm()})
+
 
 def posting_exists(url, company):
     postings = Posting.objects.filter(company=company)
@@ -64,16 +43,20 @@ def posting_exists(url, company):
             return True
     return False
 
+
 def get_jk(url):
     parsed = urlparse(url)
-    return parse_qs(parsed.query)['jk'][0]
+    return parse_qs(parsed.query)["jk"][0]
+
 
 def posting(request, id):
     posting = get_object_or_404(Posting, pk=id)
     return render(request, "jobhunter/posting.html", {"posting": posting})
 
+
 def skills(request):
     return render(request, "jobhunter/skills.html")
+
 
 def fetch_skills(request):
     postings = Posting.objects.values("skills")
