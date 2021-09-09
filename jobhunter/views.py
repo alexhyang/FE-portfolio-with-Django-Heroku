4ce47eadeb5d5502eabbabea88a582412ca1b459
Alex Yang <alex.hyang.dev@gmail.com>
2021-09-09 16:39:37 -0700

    (job hunter) updated index page

--- jobhunter/views.py
+++ jobhunter/views.py
@@ -14,12 +14,13 @@ POSTING_PER_PAGE = 20
 
 # Create your views here.
 def index(request):
-    postings = Posting.objects.all().order_by("-id")    
+    postings = Posting.objects.all().order_by("-id")
+    count = postings.count()
     posting_paginator = Paginator(postings, POSTING_PER_PAGE)
     page_num = request.GET.get('page')
     page = posting_paginator.get_page(page_num)
     
-    return render(request, "jobhunter/index.html", {"page": page})
+    return render(request, "jobhunter/index.html", {"page": page, "count": count})
 
 
 def notes(request):
