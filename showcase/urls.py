7d3af6806984b944ad4a508003c3695c07b1254f
Alex Yang <alex.hyang.dev@gmail.com>
2021-10-21 22:53:48 -0700

    (showcase) refactored backend scripts

--- showcase/urls.py
+++ showcase/urls.py
@@ -4,7 +4,4 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("survey", views.survey, name="survey"),
-    path("tribute", views.tribute, name="tribute"),
-    path("products", views.products, name="products")
 ]
--- showcase/views.py
+++ showcase/views.py
@@ -3,12 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'showcase/index.html')
-
-def survey(request):
-    return render(request, 'showcase/survey.html')
-
-def tribute(request):
-    return render(request, "showcase/tribute.html")
-
-def products(request):
-    return render(request, "showcase/products.html")
