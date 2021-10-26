3eee8027a5e31397881242e1d41143b39c321254
Alex Yang <alex.hyang.dev@gmail.com>
2021-10-25 23:46:59 -0700

    added testing

--- showcase/tests.py
+++ showcase/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.http import response
+from django.test import TestCase, Client
 
 # Create your tests here.
+class URLTests(TestCase):
+  def test_portfolio_homepage(self):
+    c = Client()
+    response = c.get("/")
+    self.assertEqual(response.status_code, 200)
+    
+  def test_vocabulary_app_homepage(self):
+    c = Client()
+    response = c.get("/vocabulary-app/")
+    self.assertEqual(response.status_code, 200)
+    
+  def test_jobhunter_app_homepage(self):
+    c = Client()
+    response = c.get("/jobhunter-app/")
+    self.assertEqual(response.status_code, 200)
+  
